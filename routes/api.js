f0e573480f3812e71f17dd2ca24dca10e4e55316
wizeeee <wizeman22@outlook.com>
2021-11-04 02:56:05 -0400

    changes to api and server

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,33 @@ const path = require("path");
 const router = require("express").Router();
 const { v4: uuidv4 } = require("uuid");
 
+router.get("/notes", (req, res) => {
+  const allNotes = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf8")
+  );
+
+  res.json(allNotes);
+});
+
+router.post("/notes", (req, res) => {
+  const newNote = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uuidv4(),
+  };
+  const allNotes = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf8")
+  );
+
+  allNotes.push(newNote);
+  fs.writeFileSync(
+    path.join(__dirname, "../db/db.json"),
+    JSON.stringify(allNotes),
+    "utf8"
+  );
+  res.json(newNote);
+});
+
 router.delete("/notes/:id", (req, res) => {
   const allNotes = JSON.parse(
     fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf8")
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const PORT = process.env.PORT || 4141;
 const app = express();
-
+const apiRoutes = require("./routes/api");
 const htmlRoutes = require("./routes/html");
 
 app.use(express.urlencoded({ extended: true }));
