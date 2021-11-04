c12810b30000e0e81a9ad6a5cd059d0d2a4ed8a0
wizeeee <wizeman22@outlook.com>
2021-11-04 02:53:17 -0400

    changes to the server

--- routes/api.js
+++ routes/api.js
@@ -3,4 +3,17 @@ const path = require("path");
 const router = require("express").Router();
 const { v4: uuidv4 } = require("uuid");
 
+router.delete("/notes/:id", (req, res) => {
+  const allNotes = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf8")
+  );
+  const newNotes = allNotes.filter((note) => note.id !== req.params.id);
+  fs.writeFileSync(
+    path.join(__dirname, "../db/db.json"),
+    JSON.stringify(newNotes),
+    "utf8"
+  );
+  res.json({ ok: true });
+});
+
 module.exports = router;
